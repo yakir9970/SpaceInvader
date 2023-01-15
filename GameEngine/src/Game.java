67c4e83cf236d8471e0361f93d815859642cc9fc
yakir9970 <yakirafeka@gmail.com>
2023-01-15 23:11:07 +0200

    checking commit

--- GameEngine/src/Game.java
+++ GameEngine/src/Game.java
@@ -3,8 +3,8 @@ import javax.swing.JFrame;
 
 public class Game extends JFrame {
 
-  static final int WIDTH = 640;
-  static final int HEIGHT = 480;
+  static final int WIDTH = 1280;
+  static final int HEIGHT = 720;
   
   GameStateMachine stateMachine;
   
